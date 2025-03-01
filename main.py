9489c4d43804a7bba34fc0fa9b1072117384679d
RynCav <ryancav101@gmail.com>
2025-03-01 17:17:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from params import *
+from utils import *
 from model import *
+from params import *
 
 # innilize each layer and activation function
-STEPS = [ConvolutionLayer(32, 3, 1, 1, 1), ReLU(), PoolingLayer(2, 2), ConvolutionLayer(64, 3, 1, 1, 1),
-         PoolingLayer(2, 2), ReLU(), FlattenLayer(BATCH_SIZE), DenseLayer(100352, 128), ReLU(), DenseLayer(128, 10),
-         Softmax()]
+STEPS = [ConvolutionLayer(32, 3, 3, 1, 1, 1), BatchNormalization(), ReLU(),
+         PoolingLayer(2, 2, 2),
+         ConvolutionLayer(64, 3, 3, 32, 1, 1), BatchNormalization(), ReLU(),
+         PoolingLayer(2, 2,2),
+         FlattenLayer(),
+         DenseLayer(3136, 1000), BatchNormalization(), ReLU(), DropoutLayer(),
+         DenseLayer(1000, 500), BatchNormalization(), ReLU(), DropoutLayer(),
+         DenseLayer(500, 10), Softmax()]
 
 # create the model object
-model = Model(STEPS)
+Model = Model(STEPS, dataset=Dataset)
+
 # train the model
-model.train(Dataset, EPOCHS, BATCH_SIZE)
+Model.train(EPOCHS, BATCH_SIZE)
 
 # test the model to determine accuracy
-model.test(Dataset)
+Model.test()
 
 # save the model to the specified file1
-save(model, 'model.pickle')
+Model.save_model()
